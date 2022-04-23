431d1085ec689ffdd259a0f911efa2bc91137a63
Juan <jgarcia1545@gmail.com>
2022-04-23 15:59:58 -0700

    Area to test if our application works

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,31 @@
+const Employee = require('../lib/Employee');
+
+// Tests for Name
+test('gets employee name', () => {
+
+});
+
+// Test for ID
+test('gets employee id', () => {
+
+});
+
+// Test for Email
+test('gets employee email', () => {
+});
+
+// Test for getName()
+test('returns name from getName() ', () => {
+});
+
+// Test for getId()
+test('returns ID from getId()', () => {
+});
+
+// Test for getEmail()
+test('returns email from getEmail()', () => {
+});
+
+// Test for getRole()
+test('returns role from getRole()', () => {
+});
